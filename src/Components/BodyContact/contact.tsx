c82d5b8c9f4e5c1b2790c9ce57fdf42b581f33dd
felipenmatos <notfelipe2@hotmail.com>
2022-12-04 18:41:50 -0300

    🔁 fix: contacts component update

--- src/Components/BodyContact/contact.tsx
+++ src/Components/BodyContact/contact.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import photoProfile from "../../icons/photoBlack.jpg";
 import whatsapp from "../../icons/whatsapp.png";
 import linkedin from "../../icons/linkedin.png";
 import github from "../../icons/github-sign.png";
@@ -10,8 +9,6 @@ import behance from "../../icons/behance.png";
 function Contact() {
   return (
     <Container>
-      <Photo src={photoProfile} alt="perfil" />
-      <Subtitle>Aqui estão algumas das minhas redes de contato.</Subtitle>
       <RowIcons>
         <Link
           href="https://wa.me/5575982410325?text=Olá%20Tudo%20bem?%20Tenho%20interesse%20em%20seus%20serviços."
@@ -32,8 +29,6 @@ function Contact() {
           <Img src={behance} />
         </Link>
       </RowIcons>
-      <CellNumber>(75) 9 8241-0325</CellNumber>
-      <Email>felipe.frontend@gmail.com</Email>
     </Container>
   );
 }
@@ -46,52 +41,6 @@ const Container = styled.div`
 
   @media (max-width: 768px) {
     margin-left: -40%;
-  }
-`;
-
-const Photo = styled.img`
-  width: 200px;
-  border-radius: 100%;
-  cursor: pointer;
-
-  -webkit-transition: all 0.7s ease;
-  transition: all 0.7s ease;
-
-  :hover {
-    -webkit-transform: scale(1.2);
-    transform: scale(1.2);
-  }
-
-  @media (max-width: 768px) {
-    width: 180px;
-    border-radius: 100%;
-    cursor: pointer;
-
-    -webkit-transition: all 0.7s ease;
-    transition: all 0.7s ease;
-
-    :hover {
-      -webkit-transform: scale(1.2);
-      transform: scale(1.2);
-    }
-  }
-`;
-
-const Subtitle = styled.p`
-  margin-top: 40px;
-  font-family: "Poppins", sans-serif;
-
-  font-weight: 700;
-  font-size: 20px;
-  line-height: 19px;
-
-  color: #4a4a4a;
-
-  @media (max-width: 768px) {
-    font-weight: 700;
-    font-size: 16px;
-    line-height: 19px;
-    color: #ffffff;
   }
 `;
 
@@ -115,41 +64,6 @@ const Img = styled.img`
   :hover {
     -webkit-transform: scale(1.2);
     transform: scale(1.2);
-  }
-`;
-
-const CellNumber = styled.p`
-  margin-top: 30px;
-  margin-bottom: -10px;
-  font-family: "Poppins", sans-serif;
-  font-weight: 700;
-  font-size: 20px;
-  line-height: 19px;
-
-  color: #4a4a4a;
-
-  @media (max-width: 768px) {
-    font-weight: 700;
-    font-size: 16px;
-    line-height: 19px;
-    color: #ffffff;
-  }
-`;
-
-const Email = styled.p`
-  font-family: "Poppins", sans-serif;
-
-  font-weight: 700;
-  font-size: 20px;
-  line-height: 19px;
-
-  color: #4a4a4a;
-
-  @media (max-width: 768px) {
-    font-weight: 700;
-    font-size: 16px;
-    line-height: 19px;
-    color: #ffffff;
   }
 `;
 
