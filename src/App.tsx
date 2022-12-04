c0f775ff0a94517103295ccdad03c74f697ebaf2
felipenmatos <notfelipe2@hotmail.com>
2022-12-04 16:12:20 -0300

    ❌ fix: removal of unused routes in the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Contato from "./Pages/Contato";
 import Home from "./Pages/Home";
-import Projetos from "./Pages/Projetos";
-import Qualificacoes from "./Pages/Qualificações";
-import Sobre from "./Pages/Sobre";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-      </Routes>
-      <Routes>
-        <Route path="/Sobre" element={<Sobre />} />
-      </Routes>
-      <Routes>
-        <Route path="/Projetos" element={<Projetos />} />
-      </Routes>
-      <Routes>
-        <Route path="/Qualificacoes" element={<Qualificacoes />} />
-      </Routes>
-      <Routes>
-        <Route path="/Contato" element={<Contato />} />
       </Routes>
     </Router>
   );
